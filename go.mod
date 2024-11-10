290119d19df0b7ad83d19433bef8abd6f610badf
benkigera <benkigera@gmail.com>
2024-11-10 21:56:22 +0300

    first go commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/benkigera/price_calculator
+
+go 1.23.2
---
+++ prices/prices.go
@@ -0,0 +1,15 @@
+package prices
+
+type TaxIncludedPriceJob struct {
+	TaxRate float64
+	InputPrices  []float64
+	TaxIncludedPrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64, inputPrices []float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		TaxRate: taxRate,
+		InputPrices: inputPrices,
+		TaxIncludedPrices: make(map[string]float64),
+	}
+}
